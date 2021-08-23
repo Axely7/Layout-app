7a6946295efdeb59951df171d8621225a1e785d0
Axely7 <axely7@hotmail.com>
2021-08-23 18:50:38 -0500

    applying flexbasis

--- App.js
+++ App.js
@@ -61,6 +61,43 @@ const App = () =>  {
             </Image>
           </View>
         </View>
+
+        <Text style={styles.titulo}>Hospedajes en LA</Text>
+
+        <View
+          style={styles.listado}>
+          <View style={styles.listadoItem}>
+            <Image 
+                style={styles.mejores}
+                source={ require('./assets/img/hospedaje1.jpg')}>
+
+            </Image>
+          </View>
+
+          <View style={styles.listadoItem}>
+            <Image 
+                style={styles.mejores}
+                source={ require('./assets/img/hospedaje2.jpg')}>
+
+            </Image>
+          </View>
+
+          <View style={styles.listadoItem}>
+            <Image 
+                style={styles.mejores}
+                source={ require('./assets/img/hospedaje3.jpg')}>
+
+            </Image>
+          </View>
+          <View style={styles.listadoItem}>
+            <Image 
+                style={styles.mejores}
+                source={ require('./assets/img/hospedaje4.jpg')}>
+
+            </Image>
+          </View>
+          
+        </View>
     </ScrollView>
     </>
     
@@ -114,7 +151,16 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     marginVertical: 5
+  },
+  listado:{
+    flexDirection: 'row',
+    flexWrap:'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoItem:{
+    flexBasis:'50%'
   }
+
 });
 
 export default App;
